6823ac2bd0112bcf1d60f35a0de12c7fb0f12e63
Abhigyan <abhigya.dhakal@gmail.com>
2022-04-10 23:41:15 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //When user connects
+  console.log("A user connected");
+
+  //Take userID and socket ID from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //Send and get message
+  socket.on(
+    "sendMessage",
+    ({ senderId, recieverId, text, conversationId, name }) => {
+      const user = getUser(recieverId);
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text,
+        conversationId,
+        name,
+      });
+    }
+  );
+
+  //When user disconnects
+  socket.on("disconnect", () => {
+    console.log("A user Disconnected");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
